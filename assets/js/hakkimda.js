0f3a03c6fcfa883cee9581064a17871f6e875d7f
Andac <keremandac240@gmail.com>
2026-01-23 15:02:58 +0300

    Initial commit

---
+++ assets/js/hakkimda.js
@@ -0,0 +1,63 @@
+const root = document.documentElement;
+
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'auto';
+}
+
+// // // const topBar = document.querySelector('.top-bar');
+// // // let introDone = false;
+
+// function dynamicOpacity(text) {
+//     const rect = text.getBoundingClientRect();
+//     const windowHeight = window.innerHeight;
+
+//     const distance = Math.abs(
+//         rect.top + rect.height / 2 - windowHeight / 2
+//     );
+
+//     const maxDistance = windowHeight / 3;
+
+//     let opacity = 1 - distance / maxDistance;
+//     opacity = Math.max(0, Math.min(1, opacity));
+
+//     text.style.opacity = opacity;
+// }
+
+// window.addEventListener('load', updateGradient);
+
+// function updateGradient() {
+//     const scrollTop = window.scrollY;
+//     const maxScroll = window.innerHeight;
+
+//     const minDeg = 135;
+//     const maxDeg = 315;
+
+//     const progress = Math.min(scrollTop / maxScroll, 1);
+//     const angle = minDeg + (maxDeg - minDeg) * progress;
+
+//     root.style.setProperty("--welcome-gradient-angle", `${angle}deg`);
+// }
+
+// window.addEventListener('scroll', updateGradient);
+
+// window.addEventListener('scroll', () => {
+//     if (!topBar) return;
+
+//     const scrollY = window.scrollY;
+//     fadeDistance = window.innerHeight * 0.5; // kaç px'de tamamen görünsün
+
+//     let opacity = scrollY / fadeDistance;
+//     opacity = Math.max(0, Math.min(1, opacity));
+
+//     topBar.style.opacity = opacity;
+
+//     if (opacity > 0.1) {
+//         topBar.style.pointerEvents = 'auto';
+//     } else {
+//         topBar.style.pointerEvents = 'none';
+//     }
+// });
+
+// window.addEventListener('scroll', () => {
+//     if (!introDone) return;
+// });
